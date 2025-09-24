465bd23e9a0b144dbce7f52cc820540b918af47d
Eric Paschke <edpaschke@gmail.com>
2025-09-24 17:45:58 -0400

    first day (late commit of your work from sunday)

--- ros-gz/src/py_pubsub/py_pubsub/node.py
+++ ros-gz/src/py_pubsub/py_pubsub/node.py
@@ -2,6 +2,8 @@ import rclpy
 from rclpy.node import Node
 
 from std_msgs.msg import String
+from geometry_msgs.msg import Twist
+from std_msgs.msg import Int32
 
 
 class SeeKeys(Node):
@@ -10,36 +12,36 @@ class SeeKeys(Node):
     def __init__(self):
     
         super().__init__('see_keys')
-        turn = 0
-        drive = 0
+        self.turn = 0
+        self.drive = 0
         self.publisher_ = self.create_publisher(Twist, '/cmd_vml', 10)
         timer_period = 0.1  # seconds
         self.timer = self.create_timer(timer_period, self.action)
 
 
-        self.subscription = self.create_subscription(Int32, '/keyboard/keypress', key_pressed, 10)
+        self.subscription = self.create_subscription(Int32, '/keyboard/keypress', self.key_pressed, 10)
 
     def key_pressed(self, message):
         match message.data:
             case 87:
-                drive = 1
+                self.drive = 1
             case 65:
-                turn = 1
+                self.turn = 1
             case 68:
-                turn = -1
+                self.turn = -1
             case 83:
-                drive = -1
+                self.drive = -1
             
 
                 
     def action(self):
         
         msg = Twist()
-        msg.linear.x = drive
-        msg.angular.z = turn
+        msg.linear.x = float(self.drive)
+        msg.angular.z = float(self.turn)
         self.publisher_.publish(msg)
-        turn = 0
-        drive = 0
+        self.turn = 0
+        self.drive = 0
 
 
 
@@ -49,7 +51,7 @@ class SeeKeys(Node):
 def main(args=None):
     rclpy.init(args=args)
 
-    minimal_publisher = MinimalPublisher()
+    minimal_publisher = SeeKeys()
 
     rclpy.spin(minimal_publisher)
 
--- ros-gz/src/py_pubsub/setup.py
+++ ros-gz/src/py_pubsub/setup.py
@@ -20,6 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'pubsub = py_pubsub.node:main'
         ],
     },
 )
